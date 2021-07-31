cfe4206546d581e0179a25985fc64cd639676ff0
sushant <sushantw78@gmail.com>
2021-07-31 15:59:48 +0530

    added % sign to weather.js humidity  output

--- src/utils.js/weather.js
+++ src/utils.js/weather.js
@@ -20,7 +20,7 @@ function getLatLong(lat, long, callback) {
         } else {
             callback(
                 undefined,
-                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees out. It is ${body.current.humidity} humid.`
+                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees out. It is ${body.current.humidity}% humid.`
             );
         }
     });
